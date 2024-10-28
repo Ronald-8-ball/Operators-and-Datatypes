77f22e51e5769fffd7002aa125effbd1d4ede7f7
Ronald-8-ball <ronaldokuk462@gmail.com>
2024-10-28 18:49:51 +0100

    Python

---
+++ DEO.py
@@ -0,0 +1,10 @@
+a = 911
+b = 112
+print("911 + 112 =",a + b)
+print("911 - 112 =",a - b)
+print("911 * 112 =",a * b)
+print("911 / 112 =",a / b)
+print("911 // 112 =",a // b)
+print("911 ** 112 =",a ** b)
+print("Is 911 equal to 112",a == b)
+print("Is 911 not equal to 112",a != b)
---
+++ REO.py
@@ -0,0 +1,8 @@
+name = "kevin"
+isapupil = True
+weight = 10.33
+age = 9
+print("Kevin is a boy",type(name))
+print("Kevin is a pupil",type(isapupil))
+print("Kevin's weight is 10.33",type(weight))
+print("Kevin's age is 9",type(age))
